42502f19b2a2a5412fa4fe14bbb1feae8bc0d70b
Akhil Gupta <akhil.gupta@daffodilsw.com>
2022-09-26 17:00:43 +0530

    Worked on updating production urls

--- lib/components/reservation_bottom_sheet_widget.dart
+++ lib/components/reservation_bottom_sheet_widget.dart
@@ -95,7 +95,7 @@ class _ReservationBottomSheetWidgetState
           // select payments you need to show [Default is all, and you can choose between WEB-CARD-APPLEPAY ]
           paymentType: paymentType == 0 ? PaymentType.CARD : PaymentType.DEVICE,
           // Transaction mode
-          sdkMode: SDKMode.Sandbox);
+          sdkMode: SDKMode.Production);
     } on PlatformException {
       // platformVersion = 'Failed to get platform version.';
     }
