43fe64b8bd4b13c76bb36d63dd356ca76e7d280b
Akhil Gupta <akhil.gupta@daffodilsw.com>
2022-11-25 11:12:19 +0530

    Worked on handling last_login time

--- lib/backend/api_requests/api_calls.dart
+++ lib/backend/api_requests/api_calls.dart
@@ -57,7 +57,7 @@ class PropertyCall {
       callName: 'Property',
       apiUrl:
           '${EnvVariables.instance.strapiBaseURl}/api/properties/${propertyId}',
-      callType: ApiCallType.POST,
+      callType: ApiCallType.GET,
       headers: {
         'x-api-key': '06db164b-46ff-44e8-a0b8-c08f8eaa069f',
       },
