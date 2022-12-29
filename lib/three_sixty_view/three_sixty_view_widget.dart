ba04009a89be3c3ee4b3e461b4ba90a97f951eee
Akhil Gupta <akhil.gupta@daffodilsw.com>
2022-12-29 12:05:55 +0530

    commented code and unused imports removed from three_sixty_view_widget.dart

--- lib/three_sixty_view/three_sixty_view_widget.dart
+++ lib/three_sixty_view/three_sixty_view_widget.dart
@@ -1,11 +1,8 @@
 import 'package:manzel/common_widgets/manzel_icons.dart';
-
 import '../flutter_flow/flutter_flow_theme.dart';
 import '../flutter_flow/flutter_flow_util.dart';
 import '../flutter_flow/flutter_flow_web_view.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class ThreeSixtyViewWidget extends StatefulWidget {
   const ThreeSixtyViewWidget({
