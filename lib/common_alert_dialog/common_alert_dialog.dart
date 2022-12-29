4eaeb4c3c65a4596e9ec9c6a883867c5dc33aaa4
Akhil Gupta <akhil.gupta@daffodilsw.com>
2022-12-29 16:31:05 +0530

    commented code and unused imports removed from common_alert_dialog.dart

--- lib/common_alert_dialog/common_alert_dialog.dart
+++ lib/common_alert_dialog/common_alert_dialog.dart
@@ -1,9 +1,7 @@
 import 'package:app_settings/app_settings.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:manzel/flutter_flow/flutter_flow_theme.dart';
 import 'package:manzel/flutter_flow/flutter_flow_util.dart';
-import 'package:manzel/flutter_flow/flutter_flow_widgets.dart';
 
 class CommonAlertDialog extends StatefulWidget {
 
