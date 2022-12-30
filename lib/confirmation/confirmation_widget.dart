a781bf160a6536f9855a6176c83e28ac46013c84
Akhil Gupta <akhil.gupta@daffodilsw.com>
2022-12-30 14:23:53 +0530

    unused imports removed from confirmation_widget.dart

--- lib/confirmation/confirmation_widget.dart
+++ lib/confirmation/confirmation_widget.dart
@@ -1,13 +1,9 @@
-import 'package:manzel/common_widgets/manzel_icons.dart';
-
 import '../flutter_flow/flutter_flow_theme.dart';
 import '../flutter_flow/flutter_flow_util.dart';
 import '../flutter_flow/flutter_flow_widgets.dart';
 import '../flutter_flow/custom_functions.dart' as functions;
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:google_fonts/google_fonts.dart';
+
 
 class ConfirmationWidget extends StatefulWidget {
   const ConfirmationWidget({
